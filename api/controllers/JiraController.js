66faaee0598e4debdb3bfaf3dbd9ad305e4a8e24
Luke H <lukemedway.uk@gmail.com>
2016-05-26 16:33:18 +0100

    updates to include typeahead

--- api/controllers/JiraController.js
+++ api/controllers/JiraController.js
@@ -5,9 +5,6 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-/*/
-    REST API NEEDS TO BE WRITTEN AS A SERVICE
-/*/
 
 
 module.exports = {
@@ -36,49 +33,5 @@ module.exports = {
         });
     }
     
-    
-    /**************************************************************
-     * 
-     * 
-     **************************************************************
-    
-    test: function(req, res) {
-        var https = require('https');
-        var https = require('https'), options = {
-            host : "redantinternal.atlassian.net",
-            port : 443,
-            path : "/rest/api/2/search?jql=key=OAS-200",
-            method : 'GET',
-            headers: { "Authorization": "Basic ###" }
-        };
-    
-        https.request(options, function(response) {
-            var responseData = '';
-            response.setEncoding('utf8');
-
-            response.on('data', function(chunk){
-                responseData += chunk;
-            });
-
-            response.once('error', function(err){
-                // Some error handling here, e.g.:
-                res.serverError(err);
-            });
-
-            response.on('end', function(){
-                try {
-                    // response available as `responseData` in `yourview`
-                    res.locals.requestData = JSON.parse(responseData);
-                } catch (e) {
-                    sails.log.warn('Could not parse response from options.hostname: ' + e);
-                }
-                
-                res.view('jiratest');
-            }); 
-
-        }).end()
-        
-    }
-    */
 }; 
 
